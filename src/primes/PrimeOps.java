463394ccf918b9f0533eb422212a70e12f2c189f
sachi <sachi@DESKTOP-BP0J2M7.mshome.net>
2020-11-26 16:52:24 -0500

    Began implementing Miller Rabin prime generation
    -Just need to finish up the prime testing logic

--- src/primes/PrimeOps.java
+++ src/primes/PrimeOps.java
@@ -3,6 +3,8 @@ package primes;
 import java.math.BigInteger;
 import java.util.Random;
 
+import utils.Constants;
+
 public class PrimeOps
 {
 	/**
@@ -33,6 +35,46 @@ public class PrimeOps
 		base2RepOfCandidate += last;
 
 		return new BigInteger(base2RepOfCandidate, 2);
+	}
+
+	/**
+	 * Uses Miller-Rabin primality test to test if the candidate is prime
+	 * 
+	 * @param candidate the BigInteger we want to check for primality
+	 * @param numTests  the number of times we should try to verify that the number
+	 *                  is prime
+	 * @return true if all the tests witness the primality of the candiate, false if
+	 *         the candidate is composite
+	 */
+	public static Boolean isPrime(BigInteger candidate, int numTests)
+	{
+		// TODO finish implementing
+
+		// Get the case where the candidate is 2 out of the way
+		if (candidate.toString().equals(BigInteger.TWO.toString())) return true;
+
+		// If the number is even, return false
+		if (candidate.mod(BigInteger.TWO).toString() == BigInteger.ZERO.toString()) return false;
+
+	}
+
+	/**
+	 * Generate a large prime number using Miller-Rabin
+	 * 
+	 * @param n the number of bits in the number
+	 * @return the prime number
+	 */
+	public static BigInteger genPrime(int n)
+	{
+		BigInteger primeCand = BigInteger.ONE;
+		Boolean isNotPrime = true;
+		while (isNotPrime)
+		{
+			primeCand = genPrimeCandidate(n);
+			isNotPrime = !isPrime(primeCand, Constants.NUM_TESTS);
+		}
+
+		return primeCand;
 	}
 
 }
