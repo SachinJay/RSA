0da496afe388cf0902435c97801b60d4d47f8b8c
sachi <sachi@DESKTOP-BP0J2M7.mshome.net>
2020-11-26 15:33:10 -0500

    Removed unnecessary methods and implemented fast exponentiation

--- src/modulus/ModOps.java
+++ src/modulus/ModOps.java
@@ -3,58 +3,8 @@ package modulus;
 public class ModOps
 {
 	/**
-	 * Reduces a number mod another number
-	 * @param a number to be reduced
-	 * @param m modulus
-	 * @return a reduced mod m
-	 */
-	public static long reduceMod(long a, long m)
-	{
-		if(m <= 0) throw new IllegalArgumentException("modulus most be positive integer");
-		
-		if(a < 0)
-		{
-			//Simply multiply by 1 mod m
-			//But to make it a positive number the one must be a negative number
-			//So multiply by 1-m which still is congruent to 1 mod m
-			
-			return reduceMod(a * (1-m), m);
-		}
-		else
-		{
-			if(a<m) return a;
-			return a%m;
-		}
-	}
-	
-	/**
-	 * Gives remainder when dividing two numbers
-	 * @param a dividend
-	 * @param b divisor
-	 * @return remainder of a/b
-	 * @throws IllegalArgumentException if a<=0, b<=0, or a < b
-	 */
-	public static long getRemainder(long a, long b)
-	{
-		if(a <= 0 || b <= 0 || a < b)
-		{
-			throw new IllegalArgumentException("First number must be >= 0\nSecond number must be >=0"
-					+ "\nFirst number must be >= second");			
-		}
-		
-		long quotient = 0;
-		long remainder = 0;
-		
-		quotient = a/b;
-		
-		remainder = a - (b*quotient);
-		
-		return remainder;
-		
-	}
-
-	/**
-	 * computes a^b mod m
+	 * computes a^b mod m using fast exponentiation.
+	 * 
 	 * @param a base
 	 * @param b exponent
 	 * @param m modulus
@@ -62,66 +12,54 @@ public class ModOps
 	 */
 	public static long fastExp(long a, long b, long m)
 	{
-		int[] arr =	breakUpExponent(b);	
-		long ans = 1L; 
-		
-		for(int i = 0; i < arr.length; i++)
+		int[] arr = breakUpExponent(b);
+		long ans = 1L;
+
+		for (int i = 0; i < arr.length; i++)
 		{
-			if(arr[i] == 1)
-			{
-				if( i == 0)
-				{
-					ans = reduceMod(a,m);
-				}
-				else
-				{
-					for(int j = 0; j < i; j++)
-					{
-						ans = reduceMod(ans * reduceMod(a*a,m),m);
-					}
-				}
-			}
+			ans *= ans;
+			ans %= m;
+			if (arr[i] == 1) ans *= a;
+			ans %= m;
 		}
-		
-		ans = reduceMod(ans, m);
+
 		return ans;
 	}
-	
-	private static int[] breakUpExponent(long exp)
+
+	/**
+	 * Converts the exponent into binary
+	 * 
+	 * @param exp the exponent
+	 * @return the binary version of the exponent in the form of an array. For
+	 *         example: exp = 19 yields [1,0,0,1,1] since 19 in binary is 10011
+	 */
+	public static int[] breakUpExponent(long exp)
 	{
+		// Get the binary string of the exponent
 		String bin = Long.toBinaryString(exp);
-		String revBin = reverseStr(bin);
-		
+
 		int[] arr = new int[bin.length()];
-		
-		for(int i = 0; i < revBin.length(); i++)
+
+		// Convert to array of integers
+		for (int i = 0; i < bin.length(); i++)
 		{
-			arr[i] = Integer.parseInt(revBin.substring(i, i+1));
-			System.out.print(arr[i] + " ");
+			arr[i] = Integer.parseInt(bin.substring(i, i + 1));
 		}
-		System.out.println();
-		
+
 		return arr;
 	}
-	
-	private static String reverseStr(String str) 
-	{
-	    StringBuilder output = new StringBuilder(str).reverse();
-	    return output.toString();
-	}
-	
-	
-	
+
 	/**
 	 * Calculates totient of m
+	 * 
 	 * @param m
-	 * @return
-//	 */
+	 * @return //
+	 */
 //	public static int totient(int m)
 //	{
 //		
 //	}
-	
+
 //	public static boolean isPrime()
 //	{
 //		
--- src/tests/ModOpsTests.java
+++ src/tests/ModOpsTests.java
@@ -1,74 +1,26 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static modulus.ModOps.breakUpExponent;
+import static modulus.ModOps.fastExp;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-import static modulus.ModOps.*;
-
 class ModOpsTests
 {
-
 	@Test
-	void remainderTests()
+	void fastExpTests()
 	{
-		//5
-		assertEquals(0, getRemainder(15, 5));
-		assertEquals(1, getRemainder(16, 5));
-		assertEquals(2, getRemainder(17, 5));
-		assertEquals(3, getRemainder(18, 5));
-		assertEquals(4, getRemainder(19, 5));
-		assertEquals(0, getRemainder(20, 5));
-		assertEquals(0, getRemainder(5, 5));
-		
-		//Ilegal inputs
-		assertThrows(IllegalArgumentException.class, () ->{
-			getRemainder(0, 7);
-		});
-		assertThrows(IllegalArgumentException.class, () ->{
-			getRemainder(7, 0);
-		});
-		assertThrows(IllegalArgumentException.class, () ->{
-			getRemainder(6, 7);
-		});
-		assertThrows(IllegalArgumentException.class, () ->{
-			getRemainder(-4, 6);
-		});
-		assertThrows(IllegalArgumentException.class, () ->{
-			getRemainder(4, -7);
-		});
-		assertThrows(IllegalArgumentException.class, () ->{
-			getRemainder(-4, -7);
-		});
+		assertEquals(0, fastExp(2L, 10001L, 2));
+		assertEquals(2800, fastExp(210776L, 19L, 496597L));
 	}
 
-	@Test
-	void reduceModTests()
+//	@Test
+	void breakUpExpTests()
 	{
-		assertEquals(0, reduceMod(0L, 5L));
-		assertEquals(1, reduceMod(1L, 5L));
-		assertEquals(2, reduceMod(2L, 5L));
-		assertEquals(3, reduceMod(3L, 5L));
-		assertEquals(4, reduceMod(4L, 5L));
-		assertEquals(0, reduceMod(5L, 5L));
-		assertEquals(1, reduceMod(6L, 5L));
-		assertEquals(2, reduceMod(7L, 5L));
-		assertEquals(3, reduceMod(8L, 5L));
-		assertEquals(4, reduceMod(9L, 5L));
-		assertEquals(0, reduceMod(10L, 5L));
-		assertEquals(4, reduceMod(-1L, 5L));
-		assertEquals(3, reduceMod(-2L, 5L));
-		assertEquals(2, reduceMod(-3L, 5L));
-		assertEquals(1, reduceMod(-4L, 5L));
-		assertEquals(0, reduceMod(-5L, 5L));
-		
-		assertEquals(457959L, reduceMod(210776L*210776L,496597L));
-		
-		
-		System.out.println(reduceMod(fastExp(210776L,4,496597L) * fastExp(210776L,4,496597L),496597L));
-		System.out.println(fastExp(210776L,8,496597L));
-		
+		System.out.println(Arrays.toString(breakUpExponent(19L)));
 	}
 
-	
 }
